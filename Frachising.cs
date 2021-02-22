65906c6adedb9ee96269403fea55de9c3b0777f0
Zilan Ang <56521248+zilannang@users.noreply.github.com>
2021-02-22 14:32:12 +0300

    Add files via upload
    Yazılım tasarım desenleri ile franchising şubeleri oluştuıruldu.

---
+++ Frachising.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TavukDünyasi
+{
+    public abstract class Frachising
+    {
+        protected double fiyat;
+
+        public virtual double getFiyat()
+        {
+            return fiyat;
+        }
+
+        protected chefPolo chefMenu;
+
+        public virtual void Chef_Polo(string tip)
+        {
+            chefPolo chef = this.chefSiparis(tip);
+            chef.Kes();
+            chef.Pisir();
+            chef.servis_Et();
+        }
+
+        protected abstract chefPolo chefSiparis(string tip);
+    }
+
+
+    public class Istanbul_Polo : Frachising
+    {
+         public Istanbul_Polo()
+        {
+            fiyat = 10;
+        }
+
+        public override double getFiyat()
+        {
+            return fiyat;
+        }
+        protected override chefPolo chefSiparis(string tip)
+        {
+            if (tip == "kekikli")
+            {
+                chefMenu = new Kekiks();
+            }
+            else if (tip == "barbekulu")
+            {
+                chefMenu = new Barbeku();
+            }
+
+            return chefMenu;
+        }
+    }
+
+    public class Yalova_Polo : Frachising
+    {
+        public Yalova_Polo()
+        {
+            fiyat = 15;
+        }
+
+        public override double getFiyat()
+        {
+            return fiyat;
+        }
+
+        protected override chefPolo chefSiparis(string tip)
+        {
+            if (tip == "biberli")
+            {
+                chefMenu = new Biberiye();
+            }
+            else if (tip == "begendili")
+            {
+                chefMenu = new Begendi();
+            }
+
+            return chefMenu;
+        }
+    }
+
+    public class Kocaeli_Polo : Frachising
+    {
+        public Kocaeli_Polo()
+        {
+            fiyat = 13;
+        }
+
+        public override double getFiyat()
+        {
+            return fiyat;
+        }
+
+        protected override chefPolo chefSiparis(string tip)
+        {
+            if (tip == "karısık")
+            {
+                chefMenu = new Anadolu();
+            }
+            else if (tip == "acili")
+            {
+                chefMenu = new Kozbi();
+            }
+
+            return chefMenu;
+        }
+    }
+}
---
+++ chefPolo.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TavukDünyasi
+{
+    public abstract class chefPolo
+    {
+        protected double fiyat;
+
+        public virtual double getFiyat()
+        {
+            return fiyat;
+        }
+
+        public virtual void Kes()
+        {
+            Console.WriteLine("Chef Polo kesildi");
+        }
+
+        public virtual void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Chef Polo pisirildi");
+        }
+
+        public virtual void servis_Et()
+        {
+            Console.WriteLine("Chef Polo servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+
+    public class Kekiks : chefPolo
+    {
+       
+
+        public override void Kes()
+        {
+            Console.WriteLine("Tavuklar küp küp kesildi");
+        }
+
+        public override void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Tavuklar güvecte pisirildi");
+        }
+
+        public override void servis_Et()
+        {
+            Console.WriteLine("Kekiks servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+
+    public class Biberiye : chefPolo
+    {
+        public override void Kes()
+        {
+            Console.WriteLine("Tavuklar uzun uzun kesildi");
+        }
+
+        public override void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Tavuklar kisik ateste pisirildi");
+        }
+
+        public override void servis_Et()
+        {
+            Console.WriteLine("Biberiye servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+
+    public class Anadolu : chefPolo
+    {
+        public override void Kes()
+        {
+            Console.WriteLine("Tavuklar kalın kalın kesildi");
+        }
+
+        public override void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Tavuklar harlı ateste pisirildi");
+        }
+
+        public override void servis_Et()
+        {
+            Console.WriteLine("Anadolu servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+
+    public class Begendi : chefPolo
+    {
+        public override void Kes()
+        {
+            Console.WriteLine("Tavuklar begendiyle kesildi");
+        }
+
+        public override void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Tavuklar haslanarak pisirildi");
+        }
+
+        public override void servis_Et()
+        {
+            Console.WriteLine("Begendi servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+
+    public class Kozbi : chefPolo
+    {
+        public override void Kes()
+        {
+            Console.WriteLine("Tavuklar ince ince kesildi");
+        }
+
+        public override void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Tavuklar fırında pisirildi");
+        }
+
+        public override void servis_Et()
+        {
+            Console.WriteLine("Közbi servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+
+    public class Barbeku : chefPolo
+    {
+        public override void Kes()
+        {
+            Console.WriteLine("Tavuklar köfte gibi kesildi");
+        }
+
+        public override void Pisir()
+        {
+            Ocak o = Ocak.ocakGetir();
+            o.doldur();
+            o.pisir();
+            Console.WriteLine("Tavuklar güvecte pisirildi");
+        }
+
+        public override void servis_Et()
+        {
+            Console.WriteLine("Barbeku servis edildi");
+            Ocak o = Ocak.ocakGetir();
+            o.bosalt();
+        }
+    }
+}
